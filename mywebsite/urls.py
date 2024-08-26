9f4349c07caccc8adc106234c25f163db48cedbe
Joy <jj0985788718@gmail.com>
2024-08-26 23:47:22 +0800

    dotenv module not found

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -20,7 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from core.views import index, contact
 
 urlpatterns = [
     path('', include('core.urls')),
